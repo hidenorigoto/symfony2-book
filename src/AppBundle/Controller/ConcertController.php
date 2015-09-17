357db07621d87fef1f187351cf880901a331949a
hidenorigoto <hidenorigoto@gmail.com>
2015-09-18 00:25:07 +0900

    [3-8-before] Concertコントローラの作成

---
+++ src/AppBundle/Controller/ConcertController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ConcertController extends Controller
+{
+}
---
+++ src/AppBundle/Tests/Controller/ConcertControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConcertControllerTest extends WebTestCase
+{
+}

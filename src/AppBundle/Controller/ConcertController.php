9cce3fb5a3b573f6d0a92fa85a62a6119a4689ae
hidenorigoto <hidenorigoto@gmail.com>
2015-09-18 01:03:35 +0900

    [3-15] 公演情報配列を修正

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -16,27 +16,32 @@ class ConcertController extends Controller
             [
                 'date' => '2015年5月3日',
                 'time' => '14:00',
-                'place' => '東京文化会館 (満席)',
+                'place' => '東京文化会館',
+                'available' => false,
             ],
             [
                 'date' => '2015年7月12日',
                 'time' => '14:00',
                 'place' => '鎌倉芸術館',
+                'available' => true,
             ],
             [
                 'date' => '2015年9月20日',
                 'time' => '15:00',
                 'place' => '横浜みなとみらいホール',
+                'available' => true,
             ],
             [
                 'date' => '2015年11月8日',
                 'time' => '15:00',
-                'place' => 'よこすか芸術劇場 (満席)',
+                'place' => 'よこすか芸術劇場',
+                'available' => false,
             ],
             [
                 'date' => '2016年1月10日',
                 'time' => '14:00',
                 'place' => '渋谷公会堂',
+                'available' => true,
             ],
         ];
 

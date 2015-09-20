135535de58ca54358a810f7831898ce1b03fce64
hidenorigoto <hidenorigoto@gmail.com>
2015-09-20 19:06:56 +0900

    [4-5-after-webassets] CSS/JSファイル群を追加

---
+++ web/js/custom.js
@@ -0,0 +1,31 @@
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        main_fun: function () {
+            //ADD REMOVE PADDING CLASS ON SCROLL
+            $(window).scroll(function () {
+                if ($(".navbar").offset().top >50) {
+                    $(".navbar-fixed-top").addClass("navbar-pad-original");
+                } else {
+                    $(".navbar-fixed-top").removeClass("navbar-pad-original");
+                }
+            });
+        }
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+}(jQuery));

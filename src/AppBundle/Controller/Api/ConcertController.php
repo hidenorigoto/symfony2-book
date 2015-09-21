8462c5af544dd918e2deadb906431d57cd613ab1
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 15:03:12 +0900

    [7-25] 公演情報APIエンドポイントのAPIドキュメント設定

--- src/AppBundle/Controller/Api/ConcertController.php
+++ src/AppBundle/Controller/Api/ConcertController.php
@@ -2,9 +2,15 @@
 namespace AppBundle\Controller\Api;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class ConcertController extends FOSRestController
 {
+    /**
+     * @ApiDoc(
+     *    description="公演情報の一覧を取得します"
+     * )
+     */
     public function getConcertsAction()
     {
         $em = $this->get('doctrine')->getManager();

4ad7888495ed6e3d5e5bca0ae0c371ccb38b79b7
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 17:28:32 +0900

    Revert "[9-5] MemberCollectionの失敗"
    This reverts commit 1f7787f87b46e94d8b5fcb4df42639ebb5a01817.

--- src/AppBundle/Entity/MemberCollection.php
+++ src/AppBundle/Entity/MemberCollection.php
@@ -7,8 +7,7 @@ class MemberCollection extends ArrayCollection
 {
     public function addMember($name, $part, $joinedDate)
     {
-        $port = '';
-        $member = new Member($name, $port, $joinedDate);
+        $member = new Member($name, $part, $joinedDate);
         $this->add($member);
     }
 }

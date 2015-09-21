29fb521d4a8224015062028c58d84c159fbc6e17
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 17:42:17 +0900

    [9-8] InquiryCsvBuilderTestクラスのCSVのテストを実装

--- src/AppBundle/Tests/Service/Csv/InquiryCsvBuilderTest.php
+++ src/AppBundle/Tests/Service/Csv/InquiryCsvBuilderTest.php
@@ -22,6 +22,25 @@ class InquiryCsvBuilderTest extends \PHPUnit_Framework_TestCase
      */
     public function CSVが正しく作られること()
     {
+        $inquiry1 = new Inquiry();
+        $inquiry1->setId(1);
+        $inquiry1->setName('テストあいうえお');
+        $inquiry1->setEmail('aiu@example.com');
+        $inquiry2 = new Inquiry();
+        $inquiry2->setId(2);
+        $inquiry2->setName('テストかきく けこ');
+        $inquiry2->setEmail('keko@example.com');
+        $inquiryCollection = new ArrayCollection([$inquiry1, $inquiry2]);
+        $this->inquiryRepository->expects($this->once())
+            ->method('findAllByKeyword')
+            ->willReturn($inquiryCollection);
+
+        $result = $this->SUT->build('テスト');
+
+        $expected = '1,テストあいうえお,aiu@example.com' . "\r\n" .
+                    '2,"テストかきく けこ",keko@example.com' . "\r\n";
+
+        $this->assertThat($result, $this->equalTo($expected));
     }
 
     protected function setUp()

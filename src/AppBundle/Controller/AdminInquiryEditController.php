11d3314879b8f799387c3e8feac298263fc921ac
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 10:55:57 +0900

    [6-11] createInquiryForm()メソッド

--- src/AppBundle/Controller/AdminInquiryEditController.php
+++ src/AppBundle/Controller/AdminInquiryEditController.php
@@ -14,4 +14,23 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class AdminInquiryEditController extends Controller
 {
+    private function createInquiryForm($inquiry)
+    {
+        return $this->createFormBuilder($inquiry,
+            ["validation_groups" => ["admin"]])
+            ->add('processStatus', 'choice', [
+                'choices' => [
+                    '未対応',
+                    '対応中',
+                    '対応済',
+                ],
+                'empty_data' => 0,
+                'expanded' => true,
+            ])
+            ->add('processMemo', 'textarea')
+            ->add('submit', 'submit', [
+                'label' => '保存',
+            ])
+            ->getForm();
+    }
 }

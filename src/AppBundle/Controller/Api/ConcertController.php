3301513df8abe9a7eb32dcf6a382a8b3dc9ecf89
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 14:51:58 +0900

    [7-21] getConcertsAction()メソッドの@Rest\Getアノテーションを削除

--- src/AppBundle/Controller/Api/ConcertController.php
+++ src/AppBundle/Controller/Api/ConcertController.php
@@ -2,13 +2,9 @@
 namespace AppBundle\Controller\Api;
 
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest;
 
 class ConcertController extends FOSRestController
 {
-    /**
-     * @Rest\Get("/api/concerts.{_format}")
-     */
     public function getConcertsAction()
     {
         $em = $this->get('doctrine')->getManager();

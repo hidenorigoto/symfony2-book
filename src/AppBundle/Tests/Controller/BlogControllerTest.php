950d474e79c8aa5676f97cda3fb35a9a1e1905b9
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 17:20:34 +0900

    [9-1-after] 不要なファンクショナルテストの削除

--- src/AppBundle/Tests/Controller/BlogControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AppBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class BlogControllerTest extends WebTestCase
-{
-}
--- src/AppBundle/Tests/Controller/ToppageControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AppBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ToppageControllerTest extends WebTestCase
-{
-}

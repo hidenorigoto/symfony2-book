bd5513818e7d38751b0921581188f1737be40ee5
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 19:57:53 +0900

    [9-10-pre2] HautelookAliceBundleとLiipFunctionalTestBundleを有効化

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,8 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Nelmio\ApiDocBundle\NelmioApiDocBundle();
+            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
+            $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
         }
 
         return $bundles;

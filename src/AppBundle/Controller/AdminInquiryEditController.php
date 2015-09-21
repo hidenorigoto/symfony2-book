6f114a419330be0ef637074b5f51b7c897445695
hidenorigoto <hidenorigoto@gmail.com>
2015-09-21 10:55:03 +0900

    [6-10] お問い合わせ編集のコントローラ

---
+++ src/AppBundle/Controller/AdminInquiryEditController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Inquiry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/admin/inquiry")
+ */
+class AdminInquiryEditController extends Controller
+{
+}
